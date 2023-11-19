ae556c1380feb098530ee368f3f86c1feea6c8f4
polinaakinshinaa <149005784+polinaakinshinaa@users.noreply.github.com>
2023-11-19 17:46:26 +0300

    Update пр1.py

--- пр1.py
+++ пр1.py
@@ -3,8 +3,8 @@ A = [2, 4, 100, 12, 9, 6]
 B = [3, 5, 2, 14, 9, 24]
 print(list(map(lambda x, y: x + y, A , B)))
 # задание 2
-A = [2, 4, 12, 24, 36, 34, 876, 48, 6, 13, 26, 39]
-result = list(filter(lambda x: (x % 12 == 0) or (x % 13 == 0), A))
+A = [2, 4, 19, 38, 57, 36, 34, 876, 48, 6, 13, 26, 39]
+result = list(filter(lambda x: (x % 19 == 0) or (x % 13 == 0), A))
 print(result)
 # задание 3
 from functools import reduce
